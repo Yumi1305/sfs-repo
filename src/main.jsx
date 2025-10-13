a2df214b40ef0c517d531889560e482519742f2b
Zhaom4 <140111794+Zhaom4@users.noreply.github.com>
2025-10-12 22:36:57 -0500

    spam 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
-
+import { CursorProvider } from './contexts/CursorContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <CursorProvider>
+        <App />
+      </CursorProvider>
     </BrowserRouter>
   </StrictMode>,
 )
